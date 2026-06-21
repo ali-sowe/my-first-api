1449e89fcc8a1c304c5e60c8743ad94c3e66d2c9
Ali coder <soweali.code@gmail.com>
2026-06-21 08:55:04 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Render 🚀");
+});
+
+app.get("/api", (req, res) => {
+  res.json({
+    message: "My first deployed API",
+    status: "working"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
